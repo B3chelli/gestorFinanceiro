08d579c1a3772b52f9dde2e3dd13a5f6ddd3d12e
Gabirle Bechelli Leal <gabrielbechellileal@gmail.com>
2026-04-18 19:46:01 -0300

    Adicionando Service Worker para PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Se o seu arquivo original tinha alguma configuração aqui dentro, pode colar de volta.
+  // Se não tinha nada, pode deixar vazio assim mesmo.
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
